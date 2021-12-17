e4641800adbade51ad6b2c0747f1b7c13d70528d
miguelangelnavarromarti <miquelangelnavarromarti@hotmail.com>
2021-12-17 13:10:46 +0100

    First commit

---
+++ Backend_private/src/main/java/coworking/backend_private/BackendPrivateApplication.java
@@ -0,0 +1,13 @@
+package coworking.backend_private;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPrivateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPrivateApplication.class, args);
+    }
+
+}
---
+++ Backend_private/src/test/java/coworking/backend_private/BackendPrivateApplicationTests.java
@@ -0,0 +1,13 @@
+package coworking.backend_private;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPrivateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
