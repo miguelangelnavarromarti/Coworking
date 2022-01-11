643cbdb9a584f3505c8e61a7fa7cd7d609d38cbe
miguelangelnavarromarti <miquelangelnavarromarti@hotmail.com>
2022-01-11 09:23:13 +0100

    Sprint 1 - Crear ClientesController

--- Backend_private/src/main/java/coworking/backend_private/Controller/ClientesController.java
+++ Backend_private/src/main/java/coworking/backend_private/Controller/ClientesController.java
@@ -1,2 +1,10 @@
-package coworking.backend_private.Controller;public class ClientesController {
+package coworking.backend_private.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ClientesController {
+    @GetMapping("clientes")
+    public String getClientes(){ return "clientes"; }
 }
