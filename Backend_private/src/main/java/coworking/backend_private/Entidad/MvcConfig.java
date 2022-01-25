0835f1947e298679e687da231f9d00166585a8e9
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-01-25 18:12:21 +0100

    Sprint 2 JC - Configuracio properties Solucionat, falta mirar bbdd  i controlador per no sobreescriure

--- Backend_private/src/main/java/coworking/backend_private/Entidad/MvcConfig.java
+++ Backend_private/src/main/java/coworking/backend_private/Entidad/MvcConfig.java
@@ -1,23 +1,30 @@
 package coworking.backend_private.Entidad;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+
+    //@Value("${<rutes.fotos>}")
+    //@Value("${rutes.fotos}")
+    @Value("${rutes.fotos}")
+    String rutaProperties;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/files/**")
                 //.addResourceLocations("file:/Users/joangalmesriera/Downloads/photos/");
-                .addResourceLocations("file:///E:/DAW/imgCoworking/");
+                //.addResourceLocations("file:///E:/DAW/imgCoworking/");
+                .addResourceLocations(rutaProperties);
+
+
     }
 
 }
