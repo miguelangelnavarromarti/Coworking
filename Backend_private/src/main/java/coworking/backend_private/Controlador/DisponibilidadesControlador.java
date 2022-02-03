e9e6d169706ae3f9dd76081f0870ad5e20f82298
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-03 13:56:40 +0100

    Sprint 4 JC - Select espais Disponibilitat - OK - Estils hores Disponibilitat

--- Backend_private/src/main/java/coworking/backend_private/Controlador/DisponibilidadesControlador.java
+++ Backend_private/src/main/java/coworking/backend_private/Controlador/DisponibilidadesControlador.java
@@ -1,11 +1,16 @@
 package coworking.backend_private.Controlador;
 
+import coworking.backend_private.Entidad.Espacio;
+import coworking.backend_private.Repositorio.EspaciosRepositorio;
+import coworking.backend_private.Servicio.EspaciosServicioImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/disponibilidad")
@@ -14,8 +19,13 @@ public class DisponibilidadesControlador {
     @Autowired
     DisponibilidadesRestControlador disponibilidadesRestControlador;
 
+    @Autowired
+    private EspaciosServicioImpl espaciosServicio;
+
     @GetMapping("/{codigoEspacio}/{stringDia}")
     public String verDisponibilidad(@PathVariable("codigoEspacio") Integer codigoEspacio, @PathVariable("stringDia") String stringDia, Model model) {
+        List<Espacio> espacioList = espaciosServicio.listaEspacio();
+        model.addAttribute("espacios",espacioList);
         model.addAttribute("codigoEspacio", codigoEspacio);
         model.addAttribute("stringDia", stringDia);
         return "disponibilidad/ver";
