5c399990b27fc7d7e6b66e8dc039b098dbec998b
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-07 19:43:42 +0100

    Sprint5JC - Inciar React - OK

--- Backend_public/routes/web.php
+++ Backend_public/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\FacturasController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/conexion', function () {
+    return view('conexion');
+});
+
+Route::get('/facturas', [FacturasController::class, 'index']);
+Route::post('/facturas', [FacturasController::class, 'crear']);
+Route::get('/facturas/{codigo}', [FacturasController::class, 'ver']);
+
+/*
+Route::post('/facturas/canceladas/', [FacturasController::class, 'crear'], [GestionCancelacionController::class, 'ver'])->middleware(usuari);
+Route::get('/facturas/canceladas/{codigo}', [FacturasCanceladasController::class, 'ver'])->middleware(usuari);
+
+Route::get('/datosClientes', [DatosClientesController::class, 'ver'])->middleware(usuari);
+Route::get('/datosClientes/password', [DatosClientesController::class, 'verPassword'])->middleware(usuari);
+
+Route::put('/datosClientes', [DatosClientesController::class, 'modificar'])->middleware(usuari);
+Route::put('/datosClientes/modificarPassword', [DatosClientesController::class, 'modificarPassword'])->middleware(usuari);
+
+Route::get('/opiniones', [OpinionesController::class, 'ver'])->middleware(usuari);
+Route::get('/opiniones/{codigo}', [OpinionesController::class, 'ver'])->middleware(usuari);
+Route::post('/opiniones', [OpinionesController::class, 'crear'])->middleware(usuari);
+Route::put('/opiniones/{codigo}', [OpinionesController::class, 'modificar'])->middleware(usuari);
+Route::delete('/opiniones/{codigo}', [OpinionesController::class, 'eliminar'])->middleware(usuari);
+*/
