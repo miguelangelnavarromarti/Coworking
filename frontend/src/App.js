8f31e7c8df658594883efe3399dd85daccf1384a
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-11 13:50:39 +0100

    Sprint5JC - Iniciar React, crear "components"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
-import axios from 'axios';
+import Facturas from './Components/Facturas';
+import Header from './Components/Header';
 
-const API = 'http://localhost:8000';
+
 
 class App extends React.Component {
 
@@ -10,64 +11,18 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      facturas: [],
-      isLoading: false,
-      error: null,
+     
     };
   }
 
-  componentDidMount() {
-
-
-    this.setState({ isLoading: true });
-
-    axios.get(API + "/facturas/1")
-      .then(result => this.setState({
-          facturas: result.data,
-          isLoading: false
-        }))
-      .catch(error => this.setState({
-        error,
-        isLoading: false
-      }));
-  }
+ 
 
   render() {
-    const { facturas, isLoading, error } = this.state;
-
-    if (error) {
-      return <p>{error.message}</p>;
-    }
-
-    if (isLoading) {
-      return <p>Loading ...</p>;
-    }
-
+   
     return (
       <div className="App">
-        <div className="container">
-          <h1>Table JSON React + Axios </h1>
-          <table className="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th>codigoCliente</th>
-                <th>codigo</th>
-                <th>Precio Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {facturas.map(function(factura,key){
-                return (
-                  <tr key={key}>
-                    <td>{factura.codigoCliente}</td>
-                    <td>{factura.codigo}</td>
-                    <td>{factura.precioTotal}</td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>      
-        </div>
+        <Header></Header>
+        <Facturas></Facturas>      
       </div>
     );
   }
---
+++ frontend/src/Components/Facturas.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const API = 'http://localhost:8000';
+ 
+class Facturas extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          facturas: [],
+          isLoading: false,
+          error: null,
+        };
+      }
+    componentDidMount() {
+
+
+    this.setState({ isLoading: true });
+
+    axios.get(API + "/facturas/1")
+        .then(result => this.setState({
+            facturas: result.data,
+            isLoading: false
+        }))
+        .catch(error => this.setState({
+        error,
+        isLoading: false
+        }));
+    }
+
+    render() {
+        const { facturas, isLoading, error } = this.state;
+
+        if (error) {
+        return <p>{error.message}</p>;
+        }
+
+        if (isLoading) {
+        return <p>Loading ...</p>;
+        }
+
+    return (
+     
+        <div className="container">
+          <h1>Table JSON React + Axios </h1>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>codigoCliente</th>
+                <th>codigo</th>
+                <th>Precio Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {facturas.map(function(factura,key){
+                return (
+                  <tr key={key}>
+                    <td>{factura.codigoCliente}</td>
+                    <td>{factura.codigo}</td>
+                    <td>{factura.precioTotal}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>      
+        </div>
+    );
+  }
+}
+ 
+export default Facturas;
