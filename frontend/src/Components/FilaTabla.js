9476b3159a76e4946e3b7b6c53052f3819da48e0
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-13 18:49:58 +0100

    Sprint5JC      - Crear primers components

---
+++ frontend/src/Components/FilaTabla.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class FilaTabla extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+        };
+      }
+    render(){
+        return(
+            <React.Fragment>
+                <td></td>
+            </React.Fragment>
+        );
+    }
+}
+
+export default FilaTabla;
---
+++ frontend/src/Components/TitulosTabla.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class TitulosTabla extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+        };
+      }
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default TitulosTabla;
