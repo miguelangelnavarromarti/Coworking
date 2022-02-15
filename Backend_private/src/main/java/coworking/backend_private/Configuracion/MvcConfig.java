34d6dad9b69f6cb415ca439e3b8c75025cc08b33
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-15 08:46:49 +0100

    Sprint5JC - Creacio log Back

---
+++ Backend_private/src/main/java/coworking/backend_private/Configuracion/MvcConfig.java
@@ -0,0 +1,54 @@
+package coworking.backend_private.Configuracion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${rutes.fotos}")
+    String rutaProperties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/files/**")
+                .addResourceLocations(rutaProperties);
+    }
+
+    @Autowired
+    @Qualifier("Interceptor")
+    private HandlerInterceptor miInterceptor;
+
+    //@Override
+    public void addInterceptor(InterceptorRegistry registry){
+        registry.addInterceptor(miInterceptor).addPathPatterns(
+                "/main/**",
+                "/bloqueos/**",
+                "/clientes/**",
+                "/disponiilidad/**",
+                "/espacios/**",
+                "/facturas/**",
+                "/facturasCanceladas/**",
+                "/gestionCancelaciones/**",
+                "/Idiomas/**",
+                "/gestionOfertas/**",
+                "/imagenes/**",
+                "/opiniones/**",
+                "/reservas/**",
+                "/tarifas/**",
+                "/tipoEspacios/**",
+                "/traduccionesEspacios/**",
+                "/traduccionesTipoEspacios/**");
+    }
+
+}
