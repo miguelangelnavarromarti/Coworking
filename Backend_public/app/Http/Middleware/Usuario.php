be9ab0929f1ac45f268b0a4931bd8f07f9b48d06
JoanCarlesRodriguez <joancarles.rs@gmail.com>
2022-02-15 13:29:03 +0100

    Sprint6JC  - Modificar una dependencia per poder fer npm install

--- Backend_public/app/Http/Middleware/Usuario.php
+++ Backend_public/app/Http/Middleware/Usuario.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\Cliente;
 
 class Usuario
 {
@@ -15,13 +16,34 @@ class Usuario
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
-    {
-        if($request->usuario == 'admin'){
+    {   
+        $jsonX = '{
+            "nombreUsuario": "JC1",
+            "password":"123456"
+            }';
+        $json = json_decode($jsonX);
+
+        $cliente = Cliente::where('nombreUsuario',$json->nombreUsuario);
+        $passwordInsertadaEncripatada = bcrypt ($json->password);
+
+        if($passwordInsertadaEncripatada == $cliente->password){
             return $next($request);
         }
         else{
             return redirect('no-autorizado');
         }
+        //---------------------------
+ /*       $cliente = Cliente::where('nombreUsuario',$request->nombreUsuario);
+        $passwordInsertadaEncripatada = bcrypt ($request->password);
+
+        if($passwordInsertadaEncripatada == $cliente->password){
+            return $next($request);
+        }
+        else{
+            return redirect('no-autorizado');
+        }
+*/
+
         //return $next($request);
         /*
         if(auth()->user()->email == 'correu que vull'){
