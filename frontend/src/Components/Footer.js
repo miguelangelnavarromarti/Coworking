0f09171e447cd172c89b495b9ec115183fd14b65
miguelangelnavarromarti <miquelangelnavarromarti@hotmail.com>
2022-03-08 11:27:48 +0100

    Corregir faltas ortograficas y diseño disponibilidad

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -29,7 +29,7 @@ class Footer extends React.Component {
                     </div>
                     <div className="col-md-5 col-12">
                         <div className="form-outline form-white mb-4">
-                            <input type="email" id="subscribe" name="subscribe" className="form-control bg-blauFluix"/>
+                            <input type="email" id="subscribe" name="subscribe" className="form-control border-groc border-2"/>
                         </div>
                     </div>
                     <div className="col-auto">
